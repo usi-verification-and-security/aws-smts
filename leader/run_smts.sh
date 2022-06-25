a998046669d8f4878ecaa5fe758b2dc434f2d427
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-06-26 01:10:59 +0200

    leader-run_smts: run 8 solver instance per node

--- leader/run_smts.sh
+++ leader/run_smts.sh
@@ -9,7 +9,7 @@ python3 smts/server/smts.py -p &
 smts/build/lemma_server -s$ip:3000 &
 
 #run solvers
-mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 1 \
+mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 8 \
   --hostfile $1 --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings \
   smts/build/solver_opensmt -s$ip:3000 -mono=$2 -satsolver="c" -cbbs=1500 -cbdf="1.0" \
   -shufinp=0.03 -shufshcls=1 -slpp=$((50000000 * 4)) \
