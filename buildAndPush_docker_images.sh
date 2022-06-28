3a9995fa1e8d949a7e9c10b445adb7add0611e8c
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-06-28 19:26:48 +0200

    Refactor

--- buildAndPush_docker_images.sh
+++ buildAndPush_docker_images.sh
@@ -6,27 +6,29 @@ if [[ $# -lt 2 ]]; then
     echo "Hint for SMTS-portfolio:  $0 smtsp <Account Number>";
     exit 1;
 fi
+t_name=$1
+a_num=$2
 
-echo "Building $1 base docker image ..."
+echo "Building ${t_name} base docker image ..."
 cd base
-docker build -t $1-base .
+docker build -t ${t_name}-base .
 
-echo "Building and Tagging $1-leader docker image ..."
+echo "Building and Tagging ${t_name}-leader docker image ..."
 cd ../leader
-docker build -t $1:leader .
-docker tag $1:leader $2.dkr.ecr.us-east-2.amazonaws.com/$1-leader
+docker build -t ${t_name}:leader .
+docker tag ${t_name}:leader ${a_num}.dkr.ecr.us-east-2.amazonaws.com/${t_name}-leader
 
-echo "Building and Tagging $1-worker docker image ..."
+echo "Building and Tagging ${t_name}-worker docker image ..."
 cd ../worker
-docker build -t $1:worker .
-docker tag $1:worker $2.dkr.ecr.us-east-2.amazonaws.com/$1-worker
+docker build -t ${t_name}:worker .
+docker tag ${t_name}:worker ${a_num}.dkr.ecr.us-east-2.amazonaws.com/${t_name}-worker
 
 echo "List Of docker images:"
 docker image ls
 
-echo "Login attempt to aws ..."
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin $2.dkr.ecr.us-east-2.amazonaws.com
+#echo "Login attempt to aws ..."
+#aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${a_num}.dkr.ecr.us-east-2.amazonaws.com
 
 echo "Push docker images to ECR ..."
-docker push $1.dkr.ecr.us-east-2.amazonaws.com/$2-worker
-docker push $1.dkr.ecr.us-east-2.amazonaws.com/$2-leader
+docker push ${a_num}.dkr.ecr.us-east-2.amazonaws.com/${t_name}-worker
+docker push ${a_num}.dkr.ecr.us-east-2.amazonaws.com/${t_name}-leader
--- leader/run_smts.sh
+++ leader/run_smts.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+server_ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
 #run smts server
-python3 smts/server/smts.py -p -el &
+echo "Server IP: "$server_ip
+echo "Instance Path: "$2
+echo "worker IPs: "
+echo $1
+cat $1
+python3 SMTS/server/smts.py -p -el &
 
 sleep 1
-
-#run lemma_server
-smts/build/lemma_server -s$ip:3000 &
-
-#run solvers
-mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 8 \
-  --hostfile $1 --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings \
-  smts/build/solver_opensmt -s$ip:3000
+SMTS/build/lemma_server -s$server_ip:3000 &
+SMTS/build/solver_opensmt -s$server_ip:3000 &
 
 #send instance
-python3 smts/server/client.py 3000 $2
+python3 SMTS/server/client.py 3000 $2
+
+#run solvers
+mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 2 \
+  --hostfile $1 SMTS/build/solver_opensmt -s$ip:3000
