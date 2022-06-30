616533dc2ecc15a733e862afb7c4c6e341bd915c
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-06-30 19:05:16 +0200

    aws-parallel22-portfolio

--- leader/run_smts.sh
+++ leader/run_smts.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-#only works in linux
-server_ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-#server_ip=$3
-
-#run smts server
-python3 SMTS/server/smts.py -p &
+#run smts
+python3 SMTS/server/smts.py -o60 -l &
 
 sleep 1
-SMTS/build/lemma_server -s$server_ip:3000 &
 
 #send instance
 python3 SMTS/server/client.py 3000 $2
-
-#run solvers
-mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 2   --map-by node:PE=2 \
-  --hostfile $1 SMTS/build/solver_opensmt -s$server_ip:3000
