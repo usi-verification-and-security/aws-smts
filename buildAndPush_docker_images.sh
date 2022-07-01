76d9c29ee4a4dc9b4bebe38718224912f6cf2eb8
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-07-01 16:34:01 +0200

    aws region should not be hard coded as it might differs on different accounts

--- buildAndPush_docker_images.sh
+++ buildAndPush_docker_images.sh
@@ -1,10 +1,11 @@
 # /bin/sh
 
-if [[ $# -lt 1 ]]; then
-    echo "Usage: $0 <Account Number>";
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <Account Number> <Region>";
     exit 1;
 fi
 a_num=$1
+region=$2
 
 echo "Building psmtscq base docker image ..."
 cd base
@@ -13,19 +14,19 @@ docker build -t psmtscq-base .
 echo "Building and Tagging psmtscq-leader docker image ..."
 cd ../leader
 docker build -t psmtscq:leader .
-docker tag psmtscq:leader ${a_num}.dkr.ecr.us-east-2.amazonaws.com/psmtscq-leader
+docker tag psmtscq:leader ${a_num}.dkr.ecr.${region}.amazonaws.com/psmtscq-leader
 
 echo "Building and Tagging psmtscq-worker docker image ..."
 cd ../worker
 docker build -t psmtscq:worker .
-docker tag psmtscq:worker ${a_num}.dkr.ecr.us-east-2.amazonaws.com/psmtscq-worker
+docker tag psmtscq:worker ${a_num}.dkr.ecr.${region}.amazonaws.com/psmtscq-worker
 
 echo "List Of docker images:"
 docker image ls
 
 echo "Login attempt to aws ..."
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${a_num}.dkr.ecr.us-east-2.amazonaws.com
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${a_num}.dkr.ecr.${region}.amazonaws.com
 
 echo "Push docker images to ECR ..."
-docker push ${a_num}.dkr.ecr.us-east-2.amazonaws.com/psmtscq-worker
-docker push ${a_num}.dkr.ecr.us-east-2.amazonaws.com/psmtscq-leader
+docker push ${a_num}.dkr.ecr.${region}.amazonaws.com/psmtscq-worker
+docker push ${a_num}.dkr.ecr.${region}.amazonaws.com/psmtscq-leader
