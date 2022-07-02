7b17a439083cc61ccaeba0ed16aa2cc5a1ce646e
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-07-02 17:49:25 +0200

    Leader: dynamically count the number of workers and pass it to the run_smts to figure out how many process should MpiRun initiate totally

--- leader/run_smts.sh
+++ leader/run_smts.sh
@@ -2,17 +2,22 @@
 
 #only works in linux
 server_ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-#server_ip=$3
+totalNumberOfSolverInstances=$(($3*8))
+echo "numberOfSolverInstances:" $totalNumberOfSolverInstances
 
 #run smts server
 python3 SMTS/server/smts.py -p &
 
 sleep 1
+#run lemma server
 SMTS/build/lemma_server -s$server_ip:3000 &
+
+#run solver clients
+mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np $totalNumberOfSolverInstances \
+  --hostfile $1 --use-hwthread-cpus --map-by node:PE=8 --bind-to none --report-bindings \
+  SMTS/build/solver_opensmt -s$server_ip:3000 &
 
 #send instance
 python3 SMTS/server/client.py 3000 $2
 
-#run solvers
-mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 2   --map-by node:PE=2 \
-  --hostfile $1 SMTS/build/solver_opensmt -s$server_ip:3000
+wait
