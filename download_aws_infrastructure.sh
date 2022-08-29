10d141a1a8ebe47b0d90b1faf55dd29b8872187f
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-08-29 16:29:38 +0200

    A simple script to clone aws infrastructure and checkout to the working version

---
+++ download_aws_infrastructure.sh
@@ -0,0 +1,4 @@
+# /bin/sh
+
+git clone https://github.com/aws-samples/aws-batch-comp-infrastructure-sample.git
+cd aws-batch-comp-infrastructure-sample && git checkout 956e80c590b14ce45af09a4cf5f7c9d42e459357
